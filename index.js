ab8a31dc0f5b5c51b978b66b5693eec90aec7714
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-06-14 09:04:12 +0530

    add start comment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+
 const fs = require('fs')
+
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 // web server
 const app = express()
@@ -56,7 +61,7 @@ function getReq() {
 }
 
 app.listen(port, () => {
-  console.log('web server started')
+  console.log(`web server started in the port ${process.env.PORT}.`)
   postReq()
   getReq()
 })
