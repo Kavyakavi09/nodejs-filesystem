0b830fc932774cfccabbe38bd1515fdff11d0eeb
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-10-01 12:57:01 +0530

    done

--- index.js
+++ index.js
@@ -1,75 +1,75 @@
-const express = require('express')
+const express = require('express');
 
-const fs = require('fs')
+const fs = require('fs');
 
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
-dotenv.config()
+dotenv.config();
 
 // web server
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
-app.use(express.json())
+app.use(express.json());
 
 // homepage
 app.get('/', (req, res) => {
-  res.send("Append URL with '/getfile' to get response")
-})
+  res.send("Append URL with '/getfile' to get response");
+});
 
 // create the text file with timestamp-------------post rquest---------------------
 function postReq() {
   app.post('/createfile', (req, res) => {
     // current date with time
 
-    let timestamp = Date.now()
+    let timestamp = Date.now();
 
-    const dateObject = new Date()
-    var date = `0${dateObject.getDate()}`.slice(-2)
-    var month = `0${dateObject.getMonth() + 1}`.slice(-2)
-    var year = dateObject.getFullYear()
-    var hours = dateObject.getHours()
-    var minutes = dateObject.getMinutes()
-    var seconds = dateObject.getSeconds()
-    var dateTime = `${date}.${month}.${year}--${hours}.${minutes}.${seconds}`
+    const dateObject = new Date();
+    var date = `0${dateObject.getDate()}`.slice(-2);
+    var month = `0${dateObject.getMonth() + 1}`.slice(-2);
+    var year = dateObject.getFullYear();
+    var hours = dateObject.getHours();
+    var minutes = dateObject.getMinutes();
+    var seconds = dateObject.getSeconds();
+    var dateTime = `${date}.${month}.${year}--${hours}.${minutes}.${seconds}`;
 
     fs.writeFile(`${dateTime}.txt`, `${timestamp}`, (err) => {
-      if (err) throw err
-      console.log('File has been created')
-    })
-    res.send(`Your text file ${dateTime}.txt is created`)
-  })
+      if (err) throw err;
+      console.log('File has been created');
+    });
+    res.send(`Your text file ${dateTime}.txt is created`);
+  });
 }
 
 // retrieves all text files--------------get request--------------------
-var path = require('path')
+var path = require('path');
 
 function getReq() {
   app.get('/getfile', (req, res) => {
     fs.readdir('../nodejs_filesystem', (err, files) => {
-      if (err) throw err
+      if (err) throw err;
 
       files.forEach((file) => {
         if (path.extname(file) === '.txt') {
-          console.log(`The text file is:${file}`)
+          console.log(`The text file is:${file}`);
 
           fs.readFile(file, 'utf-8', (err, data) => {
-            console.log(`${file} content is: ${data}`)
-          })
+            console.log(`${file} content is: ${data}`);
+          });
         }
-      })
-    })
+      });
+    });
 
-    res.send(`We have retrived all the text files.`)
-  })
+    res.send(`We have retrived all the text files.`);
+  });
 }
 
 app.listen(port, () => {
-  console.log(`web server started in the port ${process.env.PORT}.`)
-  postReq()
-  getReq()
-})
+  console.log(`web server started in the port ${process.env.PORT}.`);
+  postReq();
+  getReq();
+});
 
 // outputs for get method
 
@@ -85,3 +85,5 @@ app.listen(port, () => {
 // 13.06.2022and19.30.51.txt content is: 1655128851963
 // 13.06.2022--21.56.43.txt content is: 1655137603902
 // 13062022--192820.txt content is: 1655128700433
+// Export the Express API
+module.exports = app;
